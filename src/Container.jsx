66cdfdaf1830c79af952c76f008306a4ae5792ef
Michael <60555279+msheets1983@users.noreply.github.com>
2020-04-16 20:43:17 -0500

    MVP for day 2

---
+++ src/Container.jsx
@@ -0,0 +1,30 @@
+import styled from 'styled-components'
+
+const Container = styled.div`
+ 
+h1{
+    font-family: 'Montserrat', sans-serif;
+    color: pink;
+}
+
+h3{
+    color: pink;
+}
+
+img{
+height: 70vh;
+max-width: 100%;
+border-radius: 30px;
+border: white 4px solid;
+box-shadow: 0px 1px 6px -2px rgb(128, 127, 127);
+}
+
+p{
+    padding: 15px;
+    text-align: justify;
+    font-family: 'Raleway', sans-serif;
+}
+
+`
+
+export default Container
